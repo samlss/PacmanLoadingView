257a7eab2e08c02adce0d82dae3e20f58bb0e744
samlss <729717222@qq.com>
2018-07-11 15:47:52 +0800

    Improve width and height processing

--- library/src/main/java/com/iigo/library/PacmanLoadingView.java
+++ library/src/main/java/com/iigo/library/PacmanLoadingView.java
@@ -1,4 +1,4 @@
-package com.iigo.library;
+﻿package com.iigo.library;
 
 import android.animation.ValueAnimator;
 import android.annotation.TargetApi;
@@ -147,6 +147,7 @@ public class PacmanLoadingView extends View {
         super.onSizeChanged(w, h, oldw, oldh);
 
         eaterRadius = w < h ? w / 2 : h / 2;
+
         peaRadius = eaterRadius / 20;
         howLongToCreateNewPea = peaRadius * 10;
         peaStartXWhenCreate = w + peaRadius;
